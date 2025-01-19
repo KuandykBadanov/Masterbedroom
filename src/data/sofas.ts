783d918e94167eee52831c3984eb5ad817b9d03c
Kuandyk <badanov_1977@mail.ru>
2025-01-19 12:12:12 +0500

    add sofas

--- src/data/sofas.ts
+++ src/data/sofas.ts
@@ -16,105 +16,105 @@ export const sofas: Product[] = [
   },
   {
     id: 32,
-    name: "Диван Classic Chesterfield",
-    description: "Классический кожаный диван с каретной стяжкой",
+    name: "Диван Lupus Hall",
+    description: "Cочетание лаконичности и функциональности. Модульный дизайн, удобство и универсальность ",
     price: 198000,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1550581190-9c1c48d21d6c",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc"
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-hall-08086_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-hall-f544e-hind_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-hall-bdbd0-labi_interneti_reference.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 33,
-    name: "Диван Scandinavian",
-    description: "Скандинавский минимализм с натуральной обивкой",
+    name: "Диван Lupus Long",
+    description: "Сочетание лаконичности и функциональности. Модульный дизайн и удобство",
     price: 87500,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e",
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc"
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-oranz-b5864-hind_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-oranz-1c650_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-lupus-oranz-b75c0-labi_interneti_reference.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 34,
-    name: "Диван Loft Industrial",
-    description: "Индустриальный стиль с кожаной обивкой",
+    name: "Диван Halley",
+    description: "Космический комфорт и яркий дизайн",
     price: 145000,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e"
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-halley-2-hall-19a0e-soodsam_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-halley-2-hall-bab69_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-halley-2-hall-56e27-labi_interneti_reference.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 35,
-    name: "Диван Comfort Plus",
-    description: "Раскладной диван с ортопедическим матрасом",
+    name: "Диван Cassini",
+    description: "воплощение утончённой роскоши. Элегантные пропорции и премиальные материалы",
     price: 156000,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc",
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e"
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-cassini-kollane-fd2b3-hind_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-cassini-kollane-ff8b1_reference.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/diivan-windsor-co-cassini-kollane-9592c-labi_interneti_reference.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 36,
-    name: "Диван Art Deco",
-    description: "Роскошный диван в стиле ар-деко",
+    name: "Диван Miley Home",
+    description: "Современная классика. Лёгкий дизайн и мягкость ",
     price: 235000,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1550581190-9c1c48d21d6c",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc"
+      "https://github.com/KuandykBadanov/images/blob/main/sofa-micadoni-home-miley-miley-259x85x74-cm-60c8b-pigiau.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/sofa-micadoni-home-miley-miley-259x85x74-cm-c08c1.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/sofa-micadoni-home-miley-miley-259x85x74-cm-251a4-kaina.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 37,
-    name: "Диван Minimal",
-    description: "Компактный диван для современного интерьера",
+    name: "Диван Miley бежевый",
+    description: "Современная классика. Лёгкий дизайн и мягкость ",
     price: 78900,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e",
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc"
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_RC_51_F1_MILEY6-BG_2400x1796.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_RC_51_F1_MILEY6-1_1200x898.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_RC_51_F1_MILEY6-3_640x480.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 38,
-    name: "Диван Family Premium",
-    description: "Большой семейный диван с шезлонгом",
+    name: "Диван Miley Белый",
+    description: "Современная классика. Лёгкий дизайн и мягкость",
     price: 189000,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e"
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_U_51_F1_MILEY1-BG_1200x898.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_U_51_F1_MILEY1-1_1200x898.jpg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/Micadoni-MIC_U_51_F1_MILEY1-4_640x480_crop.jpg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
   {
     id: 39,
-    name: "Диван Urban Style",
-    description: "Современный диван с USB-зарядкой",
+    name: "Диван Bobo",
+    description: "Стильный минимализм идеальный выбор для современного интерьера",
     price: 167500,
     category: "sofas",
     images: [
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc",
-      "https://images.unsplash.com/photo-1550254478-ead40cc54513",
-      "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e"
+      "https://github.com/KuandykBadanov/images/blob/main/INTERMEDE_2021-1.jpeg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/intermede_c5p_pers01.jpeg?raw=true",
+      "https://github.com/KuandykBadanov/images/blob/main/Intermede_canape_front_liste.jpeg?raw=true"
     ],
     whatsappLink: "https://wa.me/+1234567890"
   },
