b2693b2dc17ba2c52347be4cd798bf312c35bb01
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2025-01-29 16:50:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Masterbedroom/',
+  base: './',
   publicDir: 'public', // Указывает Vite, где искать статические файлы
   optimizeDeps: {
     exclude: ['lucide-react'],
